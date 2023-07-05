2510a19cb10611ad1a5f0964a52460ed28410642
Doriane Lollia <doriane.lollia@3wa.io>
2023-07-05 17:14:17 +0200

    authentification midleware

---
+++ Middlewares/authToken.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../Models/UserM");
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = { authenticateToken };
